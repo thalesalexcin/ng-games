e3a3f368a8072de53686c9abd69e93a656975b61
thalesalexcin <thalesalexcin@gmail.com>
2026-01-05 14:39:05 +0100

    Adding basic header and footer

--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameTestComponent } from './components/game-test/game-test';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GameTestComponent],
+  imports: [RouterOutlet, GameTestComponent, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
