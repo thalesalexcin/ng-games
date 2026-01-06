0424d64e8711df9d41c0fb54a5fab65a54d3cada
thalesalexcin <thalesalexcin@gmail.com>
2026-01-06 10:35:24 +0100

    Removing unwanted test files

--- project/src/app/classes/default-controller.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DefaultController } from './default-controller';
-
-describe('DefaultController', () => {
-  it('should create an instance', () => {
-    expect(new DefaultController()).toBeTruthy();
-  });
-});
--- project/src/app/classes/input-controller.spec.ts
+++
@@ -1,7 +0,0 @@
-import { InputController } from './input-controller';
-
-describe('InputController', () => {
-  it('should create an instance', () => {
-    expect(new InputController()).toBeTruthy();
-  });
-});
