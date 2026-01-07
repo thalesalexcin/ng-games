a370b60a50300ff660eebd44090f1e0b7db3cbe7
thalesalexcin <thalesalexcin@gmail.com>
2026-01-07 22:43:02 +0100

    Optimizing by using Uint8Array instead of Grid<boolean>
    10ms to 7ms (30%)
    or 34 ms to 7ms (79%)

--- project/src/app/classes/game-of-life.ts
+++ project/src/app/classes/game-of-life.ts
@@ -2,11 +2,10 @@ import { inject } from '@angular/core';
 import { RandomService } from '../services/random-service';
 import { Point } from '../models/point';
 import { GridCoords } from '../models/grid-coords';
-import { Grid } from './utils/grid';
 
 export class GameOfLife {
-  private currentState!: Grid<boolean>;
-  private nextState!: Grid<boolean>;
+  private currentState!: Uint8Array;
+  private nextState!: Uint8Array;
 
   private offCanvas!: OffscreenCanvas;
   private offCtx!: OffscreenCanvasRenderingContext2D;
@@ -16,10 +15,17 @@ export class GameOfLife {
   private columns: number;
 
   private randomService: RandomService;
-  private hoverPosition?: GridCoords;
+  private hoverIndex?: number;
 
   setHoverWorldPosition(worldPosition?: Point) {
-    this.hoverPosition = worldPosition ? this.worldToGridPos(worldPosition) : undefined;
+    if (worldPosition) {
+      const hoverGridPos = this.worldToGridPos(worldPosition);
+      this.hoverIndex = hoverGridPos.row * this.columns + hoverGridPos.column;
+    } else {
+      this.hoverIndex = undefined;
+    }
+    const hoverGridPos = worldPosition ? this.worldToGridPos(worldPosition) : undefined;
+    if (hoverGridPos) this.hoverIndex = hoverGridPos.row * this.columns + hoverGridPos.column;
   }
 
   constructor(private width: number, private height: number, private worldOffset: Point) {
@@ -36,8 +42,8 @@ export class GameOfLife {
   }
 
   reset() {
-    this.currentState = new Grid<boolean>(this.rows, this.columns, false);
-    this.nextState = new Grid<boolean>(this.rows, this.columns, false);
+    this.currentState = new Uint8Array(this.rows * this.columns);
+    this.nextState = new Uint8Array(this.rows * this.columns);
 
     this.imageBuffer = this.offCtx.createImageData(this.width, this.height);
     for (let i = 0; i < this.currentState.length; i++) {
@@ -48,11 +54,16 @@ export class GameOfLife {
   }
 
   toggleAlive(worldPos: Point): void {
-    let gridCoords = this.worldToGridPos(worldPos);
+    let coords = this.worldToGridPos(worldPos);
 
-    if (this.currentState.isValidCoords(gridCoords)) {
-      let isAlive = this.currentState.get(gridCoords);
-      this.currentState.set(gridCoords, !isAlive);
+    let isValidCoords =
+      coords.row >= 0 &&
+      coords.row < this.rows &&
+      coords.column >= 0 &&
+      coords.column < this.columns;
+    const index = coords.row * this.columns + coords.column;
+    if (isValidCoords) {
+      this.currentState[index] = this.currentState[index] == 1 ? 0 : 1;
     }
   }
 
@@ -70,7 +81,7 @@ export class GameOfLife {
 
   fillRandom() {
     for (let i = 0; i < this.currentState.length; i++) {
-      this.currentState.setAtIndex(i, this.randomService.rnd() * 100 > 93);
+      this.currentState[i] = this.randomService.rnd() * 100 > 93 ? 1 : 0;
     }
   }
 
@@ -86,29 +97,30 @@ export class GameOfLife {
       const columnsInv = 1 / columns;
       let aliveCount = 0;
       for (let i = 0; i < this.currentState.length; i++) {
-        const currentRow = Math.floor(i * columnsInv);
-        const currentColumn = i - columns * Math.floor(i * columnsInv);
+        const currentRow = ~~(i * columnsInv);
+        const currentColumn = i - columns * currentRow;
 
         const rowLeft = (currentRow - 1) * columns;
         const rowRight = (currentRow + 1) * columns;
+        const rowCurrent = currentRow * columns;
         const columnUp = currentColumn - 1;
         const columnDown = currentColumn + 1;
-        var dataRef = this.currentState.data;
+        var dataRef = this.currentState;
         aliveCount =
-          (dataRef[rowLeft + columnUp] ? 1 : 0) +
-          (dataRef[rowLeft + currentColumn] ? 1 : 0) +
-          (dataRef[rowLeft + columnDown] ? 1 : 0) +
-          (dataRef[currentRow * columns + columnUp] ? 1 : 0) +
-          (dataRef[currentRow * columns + columnDown] ? 1 : 0) +
-          (dataRef[rowRight + columnUp] ? 1 : 0) +
-          (dataRef[rowRight + currentColumn] ? 1 : 0) +
-          (dataRef[rowRight + columnDown] ? 1 : 0);
+          dataRef[rowLeft + columnUp] +
+          dataRef[rowLeft + currentColumn] +
+          dataRef[rowLeft + columnDown] +
+          dataRef[rowCurrent + columnUp] +
+          dataRef[rowCurrent + columnDown] +
+          dataRef[rowRight + columnUp] +
+          dataRef[rowRight + currentColumn] +
+          dataRef[rowRight + columnDown];
 
-        this.nextState.data[i] = aliveCount == 3 || (dataRef[i] && aliveCount == 2);
+        this.nextState[i] = aliveCount == 3 || (dataRef[i] && aliveCount == 2) ? 1 : 0;
       }
 
       for (let i = 0; i < this.currentState.length; i++) {
-        this.currentState.data[i] = this.nextState.data[i];
+        this.currentState[i] = this.nextState[i];
       }
       this.nextGenerationScore--;
     }
@@ -116,29 +128,14 @@ export class GameOfLife {
 
   draw(ctx: CanvasRenderingContext2D) {
     for (let i = 0; i < this.currentState.length; i++) {
-      const alive = this.currentState.data[i];
       const idx = i * 4;
-
-      let isHovered = false;
-      if (this.hoverPosition) {
-        let hoveredIndex = this.currentState.coordsToIndex(this.hoverPosition);
-        isHovered = hoveredIndex == i;
-      }
-
-      //TODO add util to convert RGB to imageBuffer data
-      if (isHovered) {
-        this.imageBuffer.data[idx] = 255;
-        this.imageBuffer.data[idx + 1] = 255;
+      if (this.hoverIndex == i) {
         this.imageBuffer.data[idx + 2] = 255;
         this.imageBuffer.data[idx + 3] = 255;
-      } else if (alive) {
-        this.imageBuffer.data[idx] = 255;
-        this.imageBuffer.data[idx + 1] = 255;
+      } else if (this.currentState[i]) {
         this.imageBuffer.data[idx + 2] = 0;
         this.imageBuffer.data[idx + 3] = 255;
       } else {
-        this.imageBuffer.data[idx] = 0;
-        this.imageBuffer.data[idx + 1] = 0;
         this.imageBuffer.data[idx + 2] = 0;
         this.imageBuffer.data[idx + 3] = 0;
       }
