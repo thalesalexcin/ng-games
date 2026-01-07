fdd8973cd6ee25ce353caa8928682bc32448ecfd
thalesalexcin <thalesalexcin@gmail.com>
2026-01-07 23:17:10 +0100

    Adding route redirection. home & not-found will redirect to game of life

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { GameOfLifePage } from './components/game-of-life-page/game-of-life-page
 const sharedRoutes: Routes = [
   { path: 'game-of-life', component: GameOfLifePage },
   { path: 'ants-path', component: AntsPathPage },
+  { path: '', component: GameOfLifePage },
+  { path: '**', redirectTo: '' },
 ];
 
 export const routes: Routes = [...sharedRoutes];
